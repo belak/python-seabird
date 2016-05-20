6b7de520c7089102a9b8a56770b069d78e42e578
Kaleb Elwert <belak@coded.io>
2016-05-20 11:01:06 -0700

    Stop double-encoding unicode titles

--- seabird/modules/url/__init__.py
+++ seabird/modules/url/__init__.py
@@ -56,7 +56,7 @@ class URLPlugin(Plugin):
 
             # lxml has an implementation of xpath, so we use that to search for
             # the title tag.
-            tree = lxml.html.fromstring(str(data))
+            tree = lxml.html.fromstring(data)
             title = tree.find(".//title")
             if title is None or title.text is None:
                 return
