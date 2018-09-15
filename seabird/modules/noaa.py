f763718d8b2763653be1b13fd5fdda3e340057f6
Kaleb Elwert <belak@coded.io>
2018-09-15 09:50:13 -0700

    Fix URL for metar and add taf command (#62)

--- seabird/modules/noaa.py
+++ seabird/modules/noaa.py
@@ -5,37 +5,40 @@ import aiohttp
 from seabird.plugin import Plugin, CommandMixin
 
 
-METAR_URL = "http://weather.noaa.gov/pub/data/observations/metar/stations/{}.TXT"
+METAR_URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations/{}.TXT"
+TAF_URL = "http://tgftp.nws.noaa.gov/data/forecasts/taf/stations/{}.TXT"
 
 
-class MetarPlugin(Plugin, CommandMixin):
+class NOAAPlugin(Plugin, CommandMixin):
+    def cmd_taf(self, msg):
+        """<station>
+
+        Returns the TAF report given an airport code
+        """
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.noaa_callback(TAF_URL, msg))
+
     def cmd_metar(self, msg):
         """<station>
 
         Returns the METAR report given an airport code
         """
         loop = asyncio.get_event_loop()
-        loop.create_task(self.metar_callback(msg))
+        loop.create_task(self.noaa_callback(METAR_URL, msg))
 
-    async def metar_callback(self, msg):
+    async def noaa_callback(self, url, msg):
         loc = msg.trailing.upper()
         if not loc.isalnum():
             self.bot.mention_reply(msg, "Not a valid airport code")
             return
 
         async with aiohttp.ClientSession() as session, session.get(
-            METAR_URL.format(loc)
+            url.format(loc)
         ) as resp:
             if resp.status != 200:
                 self.bot.mention_reply(msg, "Could not find data for station")
                 return
 
-            found = False
             data = await resp.text()
-            for line in data.splitlines():
-                if line.startswith(loc):
-                    found = True
-                    self.bot.mention_reply(msg, line)
-
-            if not found:
-                self.bot.mention_reply(msg, "No results")
+            for line in data.splitlines()[1:]:
+                self.bot.mention_reply(msg, line.strip())
